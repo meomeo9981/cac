3406823b6f2ab5e8c5326774c660b3ce0975a41f
meomeo9981 <conmeobeo9981@gmail.com>
2023-02-16 22:08:51 +0700

    commit

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'Xin chao HanhNH',
+    introDesc: `Em đừng ấn vào đây !!!`,
+    btnIntro: 'OK',
+    title: 'Hôm nay anh hơi bận, nhưng vẫn muốn làm cho em cái này',
+    desc: 'Tối t7 đi chơi với anh nhá',
+    btnYes: 'Oke bồ',
+    btnNo: 'Đeo',
+    question:'Muốn đi nhanh thì đi một mình. Muốn đi chơi thì đi cùng anh',
+    reasonPlaceholder: 'Nói gì đó tuyệt vời ik',
+    btnReply: 'Done ! 💌💌',
+    reply: 'Em đã sẵn soàng gòi!!',
+    mess: 'Tuyệt, vậy là anh sắp được gặp em. Nhớ em vãi lz. ',
+    //messDesc: '💬💬 Nhắn tin cho anh đê. 💬💬',
+    btnAccept: 'Yeu anh',
+    messLink: 'https://www.instagram.com/_thawsngoo_/'
+}
